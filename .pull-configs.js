3f78aad82ec8e9b98c61b022d0f7acd12a07215e
stagas <gstagas@gmail.com>
2022-07-31 02:14:10 +0300

    chore: repo

--- .pull-configs.js
+++ .pull-configs.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const { pullConfigs } = require('pull-configs')
 
 const local = __dirname + '/'
@@ -6,12 +7,59 @@ const remote = 'https://github.com/stagas/typescript-minimal-template/raw/main/'
 const { assign, omit, sort, merge, replace } = pullConfigs(remote, local)
 
 merge('package.json', (prev, next) => {
-  assign(prev.scripts, omit(next.scripts, ['build:min']))
+  prev.trustedDependencies ??= []
+  prev.trustedDependencies = [
+    ...new Set([...prev.trustedDependencies, ...(next.trustedDependencies ?? [])]),
+  ].sort()
+  prev.types = next.types
+  prev.scripts = next.scripts
+  prev.files = next.files
   sort(assign(prev.devDependencies, next.devDependencies))
+
+  // deprecated
+  delete prev.devDependencies['@stagas/documentation-fork']
+  delete prev.devDependencies['@rollup/plugin-commonjs']
+  delete prev.devDependencies['@stagas/sucrase-jest-plugin']
+  delete prev.devDependencies['@web/dev-server-esbuild']
+  delete prev.devDependencies['@web/dev-server-rollup']
+  delete prev.devDependencies['esbuild']
+  delete prev.devDependencies['esbuild-register']
+  delete prev.devDependencies['prettier']
+  delete prev.devDependencies['terser']
+  delete prev.devDependencies['vite-web-test-runner-plugin']
+  delete prev.devDependencies['@swc-node/jest']
+  delete prev.devDependencies['chokidar']
+  delete prev.devDependencies['jest']
+  delete prev.devDependencies['jest-browser-globals']
+  delete prev.devDependencies['ts-jest']
+  delete prev.devDependencies['ts-node']
+  delete prev.devDependencies['wtr-plugin-vite']
+  delete prev.devDependencies['@stagas/jest-node-exports-resolver']
 })
+replace('.gitattributes')
+replace('.gitignore')
+replace('.npmrc')
 replace('.eslintrc.js')
-replace('.prettierrc')
-replace('jest.config.js')
+replace('.pull-configs.js')
+replace('.swcrc')
+replace('dprint.json')
 replace('tsconfig.json')
-replace('web-test-runner.config.js')
-merge('.vscode/settings.json')
+replace('tsconfig.dist.json')
+replace('LICENSE')
+
+const deprecated = [
+  '.vscode/extensions.json',
+  '.vscode',
+  '.prettierrc',
+  '.prettierignore',
+  'example/tsconfig.json',
+  'vite.config.js',
+  'jest.config.js',
+  'web-test-runner.config.js',
+]
+deprecated.forEach(x => {
+  try {
+    fs.rmSync(x, { recursive: true })
+    console.log('removed', x)
+  } catch {}
+})
